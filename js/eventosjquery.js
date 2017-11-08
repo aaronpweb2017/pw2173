2da82a842b918f6ae90a10e317324d49e10dc2fc
aaronpweb2017@hotmail.com <luis-aaron40@hotmail.com>
2017-11-08 16:04:04 -0700

    primerJQuery

---
+++ js/eventosjquery.js
@@ -0,0 +1,15 @@
+var inicia=function(){
+	//JSON = JavaScript Object Notation.
+	$.ajax({
+  			url: 'https://randomuser.me/api/',
+  			dataType: 'json',
+  			success: function(data) {
+  				alert(data.results[0].name.first+" "+data.results[0].name.last);
+    			console.log(data);
+  			}
+	});
+	//alert("Lista la página");
+}
+//iniciamos JQuery 
+//cuando la página está lista ejecuta la función inicia.
+$(document).ready(inicia);
