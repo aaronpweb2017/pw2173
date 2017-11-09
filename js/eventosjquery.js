a5f9a85bf019715025188271c4c37259b1d0d2ae
aaronpweb2017@hotmail.com <luis-aaron40@hotmail.com>
2017-11-09 15:58:14 -0700

    commitRandomuser1

--- js/eventosjquery.js
+++ js/eventosjquery.js
@@ -1,14 +1,23 @@
 var inicia=function(){
-	//JSON = JavaScript Object Notation.
-	$.ajax({
-  			url: 'https://randomuser.me/api/',
-  			dataType: 'json',
-  			success: function(data) {
-  				alert(data.results[0].name.first+" "+data.results[0].name.last);
-    			console.log(data);
-  			}
-	});
+  var nuevo=function(){
+    //JSON = JavaScript Object Notation.
+    //$ --> JQuery.
+    $.ajax({
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+        success: function(data) {
+          //alert(data.results[0].name.first+" "+data.results[0].name.last);
+          //console.log(data);
+          //.html --> incrusta código HTML en JQuery.
+          $("#foto").attr("src", data.results[0].picture.large);
+          $("#nombre").html("nombre: "+data.results[0].name.first+" "+data.results[0].name.last);
+        }
+   });
+  }
 	//alert("Lista la página");
+  //Aquí van todos los elementos que corresonden al apágina (se encienden los eventos):
+  //$("ntnNuevo").off("click", nuevo); --> aquí no hace nada el botón.
+  $("#btnNuevo").on("click",nuevo); //El botón realiza una acción.
 }
 //iniciamos JQuery 
 //cuando la página está lista ejecuta la función inicia.
