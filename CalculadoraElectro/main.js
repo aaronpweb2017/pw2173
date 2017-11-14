8d156d1c983f17d7aa6773ff5ce98c1abc48cc15
aaronpweb2017@hotmail.com <luis-aaron40@hotmail.com>
2017-11-14 16:06:38 -0700

    CalculadoraElectroCommit01

---
+++ CalculadoraElectro/main.js
@@ -0,0 +1,21 @@
+
+
+const app=require('electron').app; //necesito un acceso al paquete electron y a la aplicación.
+const BrowserWindow = require('electron').BrowserWindow; //Da acceso al sistema de pantallas del SO (Windowsm LINUX ó MAC).
+const path=require('path');//Muestra la ruta del archivo.
+const url=require('url');//Carga una página.
+//ECMASCRIPT=6
+let PantallaPrincipal;/*let-->inicializa la constante que despues obtendra el valor el cual no cambiara por ser constante.*/
+
+function muestraPantallaPrincipal(){
+		PantallaPrincipal=new BrowserWindow({width:320, height:425});
+		PantallaPrincipal.loadURL(url.format({
+			//join --> concatena cadenas.
+			pathname: path.join(__dirname, 'index.html'),
+			protocol: 'file',//tipo de archivo.
+			slashes: true //diagonales normales.
+		}))
+		PantallaPrincipal.show;
+}
+
+app.on('ready', muestraPantallaPrincipal)//enciende la aplicación
