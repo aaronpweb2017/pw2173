b668f391061b2f4fc8d14c321682e41ab01b91a2
aaronpweb2017@hotmail.com <luis-aaron40@hotmail.com>
2017-11-23 16:05:05 -0700

    ListadoUsuariosPrintToPDF

--- Listado/detalleUsuarios.js
+++ Listado/detalleUsuarios.js
@@ -1,6 +1,15 @@
 const {BrowserWindow} = require('electron').remote;
 const app = require('electron').app;
 const $ = require('jquery');
+//Constantes para llamar al PDF:
+const ipc=require('electron').ipcRenderer
+const botonPDF=document.getElementById('btnPDF')
+//Se activa el evento click del botón btnPDF:
+botonPDF.addEventListener('click',function(event){
+	//desaparece el botón antes de que aparezca el PDF:
+	botonPDF.style.display="none" 
+	ipc.send('print-to-pdf')
+})
 
 var nombre=require('electron').remote.getGlobal('infoUsuarios').nombre;
 var genero=require('electron').remote.getGlobal('infoUsuarios').genero;
--- Listado/main.js
+++ Listado/main.js
@@ -3,6 +3,16 @@
 const {app,BrowserWindow} = require('electron');
 const path = require('path'); //Muestra la ruta del archivo
 const url = require('url'); //Carga una página
+//Constantes para PDF:
+const electron=require('electron');
+const fs=require('fs');//Sistema de archivos de la PC.
+const os = require('os');//Acceso al sistema operativo.
+//Para declarar una función remota (IPC's):
+const ipc=electron.ipcMain;//llamada a un prodecimiento interno.,
+//Para abrir el archivo PDF se debe acceder a la terminal (línea de comandos):
+const shell=electron.shell; //llamando a la terminal del OS (Operative System).
+
+
 // ECMASCRIPT = 6
 let PantallaPrincipal;
 //objeto global para compartir datos entre pantallas:
@@ -23,5 +33,25 @@ function muestraPantallaPrincipal(){
 	//PantallaPrincipal.webContents.openDevTools();
 	PantallaPrincipal.show();
 }
+//Evento para PDF (declaración): join --> para unir cadenas.
+ipc.on('print-to-pdf',function(event){ //Se trae la ventana y extrae su contenido.
+	const pdfPath=path.join(os.tmpdir(),'print.pdf')//uniendo la ruta con el nombre del archivo.
+	//obteniendo el contenido web de la ventana que se desea imprimir:
+	const win=BrowserWindow.fromWebContents(event.sender)
+	win.webContents.printToPDF({},function(error,data){
+		if(error) throw error
+		fs.writeFile(pdfPath,data,function(error){
+			if(error){
+				throw error//si hay error lanza el mismo.
+			}
+			//si todo sale bien abre el archivo construido.
+			shell.openExternal('file://'+pdfPath)
+			win.close()	
+		})
+	})
+});
 
 app.on('ready',muestraPantallaPrincipal);
+
+		
+
