e380956cf13dbb86fe199070adccdf59926767b9
aaronpweb2017@hotmail.com <luis-aaron40@hotmail.com>
2017-11-29 16:07:56 -0700

    PHPintroduction

---
+++ PHP/holaMundo.php
@@ -0,0 +1,10 @@
+<?php
+	// Hagamos el Hola Mundo:
+	// echo "Hola mundo." // maneja la pantalla y la consola.
+	//print "Hola mundo." //maneja la pura pantalla.
+	for($i=0;$i<5;$i++)
+		print "Hola mundo.<br>"
+	//Para ejecutar el código en este caso: http://localhost/pw/PHP/holaMundo.php
+?>
+
+Hacemos una Base de datos con: http://localhost/phpmyadmin/ (phpMyAdmin)
